028cfaddde0f861829486dd0344df2e7848b735c
Nirvan Nagar <nirvan.nagar@gmail.com>
2020-06-07 19:52:53 -0400

    Fixed pipeline.

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/code-nirvana-official/SwiftPackageB.git", from: "1.0.0"),
+        .package(url: "git@github.com:code-nirvana-official/SwiftPackageB.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
