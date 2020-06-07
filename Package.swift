36b1878a7a2a5ac7c0c64358f75ae92a86c8ff3a
Nirvan Nagar <nirvan.nagar@gmail.com>
2020-06-07 19:55:51 -0400

    Add swift package C.

--- Package.swift
+++ Package.swift
@@ -14,13 +14,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "git@github.com:code-nirvana-official/SwiftPackageB.git", from: "1.0.0"),
+        .package(url: "git@github.com:code-nirvana-official/SwiftPackageC.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftPackageA",
-            dependencies: ["SwiftPackageB"]),
+            dependencies: ["SwiftPackageB", "SwiftPackageC"]),
         .testTarget(
             name: "SwiftPackageATests",
             dependencies: ["SwiftPackageA"]),
